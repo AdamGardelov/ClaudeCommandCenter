6e1b2161a2929dc383419824dc61dcfc9d49448b
Adam Gärdelöv <adam.gardelov@wint.se>
2026-02-22 10:49:09 +0100

    feat: enable desktop notifications (notify-send) by default

--- Models/NotificationConfig.cs
+++ Models/NotificationConfig.cs
@@ -5,6 +5,6 @@ public class NotificationConfig
     public bool Enabled { get; set; } = true;
     public bool Bell { get; set; } = true;
     public bool OscNotify { get; set; } = false;
-    public bool DesktopNotify { get; set; } = false;
+    public bool DesktopNotify { get; set; } = true;
     public int CooldownSeconds { get; set; } = 10;
 }
