0f3e26d7a95a050e411476dbd688139d65628eb5
Adam Gärdelöv <adam.gardelov@wint.se>
2026-02-26 22:29:35 +0100

    feat: add RemoteHost model

---
+++ Models/RemoteHost.cs
@@ -0,0 +1,9 @@
+namespace ClaudeCommandCenter.Models;
+
+public class RemoteHost
+{
+    public required string Name { get; set; }
+    public required string Host { get; set; }
+    public string WorktreeBasePath { get; set; } = "~/worktrees";
+    public List<FavoriteFolder> FavoriteFolders { get; set; } = [];
+}
